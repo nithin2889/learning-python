d7ce06a46adb9e2793ff00f93903caf625c612c6
Nithin <nithin2889@gmail.com>
2020-02-24 21:53:31 +0530

    python multi-dimensional matrices

---
+++ 019-py_matrix.py
@@ -0,0 +1,21 @@
+# Matrix is a way to describe 2D lists or multi-dimensional lists
+# Matrix is an array with another array inside of it.
+# This is a 2D array where we have a main array and sub arrays inside of it (3 arrays to be exact).
+two_d_matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(two_d_matrix[2][2])
+
+three_d_matrix = [
+                     [1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9]
+                 ], \
+                 [
+                     [9, 8, 7],
+                     [6, 5, 4],
+                     [3, 2, 1]
+                 ]
+print(three_d_matrix[1][1][1])
