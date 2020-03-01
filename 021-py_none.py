3362f641ebcd35c71a9e1ee90eb219f524aca39a
Nithin <nithin2889@gmail.com>
2020-03-01 20:24:00 +0530

    python None keyword

---
+++ 021-py_none.py
@@ -0,0 +1,11 @@
+# When we talked about lists, we saw our special type "None"
+# where some methods that worked on lists didn't really produce anything.
+# They modified the lists in place, but the output of whatever they were
+# doing was this thing called "None". What does this mean?
+# If you remember, I mentioned "None" as a special data type that exists
+# in Python and most languages have something like this to represent the
+# absence of value and in some other languages you might have heard of "null".
+
+# Example:
+weapons = None
+print(weapons)
